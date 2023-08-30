b70246e9f6ecbd548399cae13b2d6a96c653a5a2
JayceFR <jaycejefferson.vicious@gmail.com>
2023-08-30 16:25:35 +0100

    Read all songs on device

--- app/src/main/java/com/example/musicify/MainActivity.java
+++ app/src/main/java/com/example/musicify/MainActivity.java
@@ -1,14 +1,98 @@
 package com.example.musicify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText song_text;
+    private static final String Tag = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        song_text = findViewById(R.id.song_text);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_MEDIA_AUDIO)){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+            else{
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+        else{
+            fetch_songs();
+        }
+    }
+    //Modify this function in the future for Recycler View
+    public void fetch_songs(){
+        ArrayList<String> songs = getMusic();
+        song_text.setText("Hello world");
+    }
+
+    public ArrayList<String> getMusic(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri songURI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor songCursor = contentResolver.query(songURI, null, null, null,null);
+        ArrayList<String> songs = new ArrayList<String>();
+        if (songCursor != null && songCursor.moveToFirst()){
+            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            MediaStore.Audio.Media.
+            int isSong = songCursor.getColumnIndex(MediaStore.Audio.Media.IS_MUSIC);
+            do {
+                String currentTitle = songCursor.getString(songTitle);
+                String currentArtist = songCursor.getString(songArtist);
+                int is_current_music = songCursor.getInt(isSong);
+                if (is_current_music == 1){
+                    Log.i(Tag, currentTitle + is_current_music);
+                    songs.add(currentTitle + currentArtist);
+                }
+
+            }while (songCursor.moveToNext());
+            /*do {
+                String currentTitle = songCursor.getString(songTitle);
+                String currentArtist = songCursor.getString(songArtist);
+                Log.i(Tag, currentTitle);
+                songs.add(currentTitle + currentArtist);
+            } while (songCursor.moveToFirst());*/
+        }
+        return songs;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission Granted! Enjoy Listening", Toast.LENGTH_SHORT).show();
+                        fetch_songs();
+                    }
+                } else {
+                    Toast.makeText(this, "No Permission Granted!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
     }
 }
